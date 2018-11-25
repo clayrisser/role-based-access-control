900fa6bbdc2d2e9a32f59e990366f9c71b9baf8a
Jam Risser <jam@codejam.ninja>
2018-11-25 01:25:19 -0600

    Added subject can tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,4 +1,17 @@
-import RoleBasedAccessControl from '../src';
+import RoleBasedAccessControl, {
+  Policy,
+  Role,
+  Statement,
+  Subject
+} from '../src';
+
+const policies = {
+  read: new Policy('read', [new Statement('get', 'post')])
+};
+
+const roles = {
+  visitor: new Role('read', [policies.read])
+};
 
 describe('RoleBasedAccessControl', () => {
   it('is class', async () => {
@@ -6,9 +19,13 @@ describe('RoleBasedAccessControl', () => {
   });
 });
 
-describe('RoleBasedAccessControl()', () => {
-  it('should do something', async () => {
-    const rbac = new RoleBasedAccessControl();
-    expect(!!rbac).toBe(true);
+describe('new Subject(name, roles).can(action, resource)', () => {
+  it('should grant user access', async () => {
+    const user = new Subject('user', [roles.visitor]);
+    expect(user.can('get', 'post')).toBe(true);
+  });
+  it('should block user access', async () => {
+    const user = new Subject('user', []);
+    expect(user.can('get', 'post')).toBe(false);
   });
 });
